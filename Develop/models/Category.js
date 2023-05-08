adefb79ab57b022f4ac29875a65b42a1ac74ed60
KnightHawk1090 <121210332+KnightHawk1090@users.noreply.github.com>
2023-05-07 22:14:42 -0500

    updated code

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // category name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
